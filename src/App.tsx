0b0468246c6a086ec4dfbd06fe23cc2d53711a7e
Justin <contactjustinzamora@gmail.com>
2022-10-08 12:30:14 -0700

    skeleton structure for app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App:React.FC = () => {
+  return (
+    <div className="App">
+      App
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/fetch/FetchApp.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FetchApp:React.FC = () => {
+
+    //find api
+    //useEffect to render api on first render
+    //put in a state and render that state
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FetchApp
